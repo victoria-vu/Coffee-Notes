c9914ff116f8a52f66f68d3771280214e74b3c26
Victoria Vu <victoriamvvu@gmail.com>
2023-02-14 23:42:07 -0800

    Added google maps to cafe details page

--- seed_database.py
+++ seed_database.py
@@ -16,30 +16,6 @@ os.system("createdb cafes")
 model.connect_to_db(server.app)
 model.db.create_all()
 
-# This will load data from data/cafes.json and save it to a variable
-with open("data/cafes.json") as f:
-    cafe_data = json.loads(f.read())
-
-# Create cafes and store them in a list
-# cafes_in_db = []
-# for cafe in cafe_data:
-#     name, address, city, state, zip_code, latitude, longitude, phone, img_url = (cafe["name"], 
-#                                                             cafe["location"]["address1"],
-#                                                             cafe["location"]["city"],
-#                                                             cafe["location"]["state"],
-#                                                             cafe["location"]["zip_code"], 
-#                                                             cafe["coordinates"]["latitude"],
-#                                                             cafe["coordinates"]["longitude"],
-#                                                             cafe["display_phone"], 
-#                                                             cafe["image_url"])
-
-#     db_cafe = crud.create_cafe(name, address, city, state, zip_code, latitude, longitude, phone, img_url)
-
-#     cafes_in_db.append(db_cafe)
-
-# model.db.session.add_all(cafes_in_db)
-# model.db.session.commit()
-
 # Create 10 users
 for n in range(10):
     email = f"user{n}@test.com"
--- static/js/cafeDetails.js
+++ static/js/cafeDetails.js
@@ -86,3 +86,158 @@ if (deleteReviewButton) {
         });
     });
 };
+
+// Google Maps API
+function initMap() {
+
+    const cafeCoords = {
+        lat: Number(document.querySelector('#map-lat').value),
+        lng: Number(document.querySelector('#map-lng').value),
+    };
+
+    const map = new google.maps.Map(document.querySelector('#map'), {
+        center: cafeCoords,
+        zoom: 13.1,
+        styles: [
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.man_made",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    },
+                    {
+                        "lightness": 20
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "hue": "#f49935"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "hue": "#fad959"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "hue": "#a1cdfc"
+                    },
+                    {
+                        "saturation": 30
+                    },
+                    {
+                        "lightness": 49
+                    }
+                ]
+            }
+        ]
+    });
+
+    const cafeName = document.querySelector('#cafe-name').innerHTML
+
+    const cafeMarker = new google.maps.Marker({
+        position: cafeCoords,
+        title: 'Cafe Name',
+        icon: {
+            url: '/static/img/coffee.png',
+            scaledSize: new google.maps.Size(40,40),
+        },
+        map: map,
+    });
+
+    const cafeInfo = new google.maps.InfoWindow();
+
+    const cafeInfoContent = `<h4>${cafeName}<h/h4>`
+
+    cafeMarker.addListener('click', () => {
+        cafeInfo.setContent(cafeInfoContent)
+        cafeInfo.open(map, cafeMarker)
+    });
+
+};  
