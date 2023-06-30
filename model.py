38bd693dccbe121bd31e90677381092d825ce342
Victoria Vu <victoriamvvu@gmail.com>
2023-06-29 21:40:38 -0700

    Create model file, postgresql database cafes_db, and User class

---
+++ model.py
@@ -0,0 +1,36 @@
+"""Models for Coffee Notes app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    fname = db.Column(db.String, nullable=False)
+    lname = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f"<User user_id={self.user_id} name={self.fname} email={self.email}>"
+    
+
+def connect_to_db(flask_app, db_uri="postgresql:///cafes_db", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from app import app
+    app.app_context().push()
+    connect_to_db(app, echo=False)
