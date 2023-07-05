ae67e6c0a3bb5e4aaba52cc8b52c8322b15674f4
Victoria Vu <victoriamvvu@gmail.com>
2023-07-05 15:12:30 -0700

    Create functions to retrieve, including get user by id and user by email

--- crud.py
+++ crud.py
@@ -41,6 +41,36 @@ def create_note(user, bookmark, note):
 ### FUNCTIONS TO RETRIEVE ###
 
 
+def get_user_by_id(user_id):
+    """Return a user by user id."""
+
+    return User.query.get(user_id)
+
+
+def get_user_by_email(email):
+    """Return a user with a given email."""
+
+    return User.query.filter(User.email == email).first()
+
+
+def get_cafe_by_id(cafe_id):
+    """Return a cafe by cafe id."""
+
+    pass
+
+
+def get_all_user_bookmarks(user_id):
+    """Returns all bookmarks for a user."""
+
+    pass
+
+
+def get_note_by_bookmark_id(bookmark_id):
+    """Returns a note by bookmark id."""
+
+    pass
+
+
 if __name__ == "__main__":
     from app import app
     connect_to_db(app)
